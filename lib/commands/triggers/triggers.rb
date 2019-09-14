be328ebd0584b4f1e9863e50eed627953c8eb0a6
Luukuton <mleinikka@gmail.com>
2019-09-14 18:09:48 +0300

    Beautified the trigger list

--- lib/commands/triggers/triggers.rb
+++ lib/commands/triggers/triggers.rb
@@ -4,13 +4,13 @@ module TohsakaBot
       extend Discordrb::Commands::CommandContainer
       command(:triggers,
               aliases: %i[listtriggers triggerlist liipaisimet liipaisinlista triggerit],
-              description: 'Lists triggers.',
+              description: "Lists user's triggers. Parameter 'all' lists all triggers.",
               usage: '',
               rescue: "Something went wrong!\n`%exception%`") do |event, all_triggers|
 
         triggers = YAML.load_file('data/triggers.yml')
         current_triggers = []
-        output = "```  ID | TRIGGER               | MSG/FILE \n===============================================\n"
+        output = "`  ID | TRIGGER               | MSG/FILE"
         pos = 0
 
         if all_triggers == "all"
@@ -23,22 +23,19 @@ module TohsakaBot
 
         sorted = triggers.sort_by { |k| k }
         sorted.each do |key, value|
-
           if (value["user"].to_s =~ author_id) == 0
-
             current_triggers << [key, value["trigger"].to_s, value["reply"].to_s, value["file"].to_s, value["user"]]
-
             if current_triggers[pos][2].empty?
-              output << "#{sprintf("%4s", current_triggers[pos][0])} | #{sprintf("%-21s", current_triggers[pos][1][0..15])} | #{current_triggers[pos][3][9..-1][0..20]}\n"
+              output << "`#{sprintf("%4s", current_triggers[pos][0])} | #{sprintf("%-21s", current_triggers[pos][1][0..15])} | #{current_triggers[pos][3][9..-1][0..40]}`\n"
             else
-              output << "#{sprintf("%4s", current_triggers[pos][0])} | #{sprintf("%-21s", current_triggers[pos][1][0..15])} | #{current_triggers[pos][2].lines.first.chomp[0..20]}\n"
+              output << "`#{sprintf("%4s", current_triggers[pos][0])} | #{sprintf("%-21s", current_triggers[pos][1][0..15])} | #{current_triggers[pos][2].lines.first.chomp[0..40]}`\n"
             end
             pos += 1
           end
         end
 
         if current_triggers.any?
-          where.split_send "#{output}```"
+          where.split_send "#{output}"
         else
           event.<< 'No triggers found.'
         end
