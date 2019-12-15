98d4f41122e274eaf5dad58468d4afafe6921bbb
Luukuton <mleinikka@gmail.com>
2019-12-15 20:16:44 +0200

    Removed an unused streaming status from nowplaying.

--- lib/async/refreshnp.rb
+++ lib/async/refreshnp.rb
@@ -4,12 +4,7 @@ module TohsakaBot
       Thread.new do
         loop do
           playing = $settings['np']
-          # 0 is Playing and anything else is Streaming
-          if playing[0] != 0
-            BOT.stream(playing[1].to_s, playing[0].to_s)
-          else
-            BOT.game = playing[1].to_s
-          end
+          BOT.game = playing.to_s
           sleep(1800)
         end
       end
