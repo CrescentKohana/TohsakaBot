dc8f4de36b8563071d3018ae7a59e8adc298fbf8
Luukuton <mleinikka@gmail.com>
2020-01-12 23:53:24 +0200

    Added support for embedded images in AskRin.

--- lib/commands/ask_rin/answer_add.rb
+++ lib/commands/ask_rin/answer_add.rb
@@ -5,11 +5,20 @@ module TohsakaBot
       command(:answeradd,
               aliases: %i[addanswer adda answer],
               description: 'Adds an answer to the list.',
-              usage: 'addanswer <answer>',
-              min_args: 1,
+              usage: 'addanswer <answer (or an embedded image)>',
               rescue: "Something went wrong!\n`%exception%`") do |event, *msg|
 
-        answer = msg.join(' ').gsub("\t", '').sanitize_string
+        if event.message.attachments.first.nil?
+          if !msg.nil?
+            answer = msg.join(' ').gsub("\t", '').sanitize_string
+          else
+            event.respond "Message or an embbedded image is required."
+            break
+          end
+        else
+          answer = event.message.attachments.first.url
+        end
+
         CSV::foreach("data/ask_rin_answers.csv", "r", :col_sep => "\t") do |row|
           if answer.include?(row[1])
             event.respond "Answer already exists. Aborting."
