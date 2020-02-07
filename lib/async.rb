d52faf2fca9d562dd7f7e2e8220ecf8579556186
Luukuton <mleinikka@gmail.com>
2020-02-07 22:26:02 +0200

    Enabled LoadAlko module.

--- lib/async.rb
+++ lib/async.rb
@@ -3,7 +3,7 @@ module TohsakaBot
     Dir["#{File.dirname(__FILE__)}/async/*.rb"].each { |file| require file }
 
     # All asynchronous events listed below are activated and in use.
-    @async = [RefreshNP, RemindOnTime, RepostCheck, TempRoleNoMore]
+    @async = [RefreshNP, RemindOnTime, RepostCheck, TempRoleNoMore, LoadAlko]
 
     def self.include!
       @async.each do |event|
