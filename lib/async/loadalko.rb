bc9b31670a6b14587af02fac10938b273bdc04cf
Luukuton <mleinikka@gmail.com>
2020-06-06 19:24:34 +0300

    Let's not update alko.csv if there hasn't been any changes in the xlsx

--- lib/async/loadalko.rb
+++ lib/async/loadalko.rb
@@ -5,21 +5,32 @@ module TohsakaBot
         url = "https://www.alko.fi/INTERSHOP/static/WFS/Alko-OnlineShop-Site/-/Alko-OnlineShop/" +
             "fi_FI/Alkon%20Hinnasto%20Tekstitiedostona/alkon-hinnasto-tekstitiedostona.xlsx"
 
+        temp_file = 'tmp/alko_temp.xlsx'
+
         loop do
-          # TODO: Compare with the existing file first.
-          IO.copy_stream(URI.open(url), 'tmp/alko_temp.xlsx')
-          sheet = Roo::Spreadsheet.open('tmp/alko_temp.xlsx')
-          sheet.to_csv('tmp/alko_temp.csv')
+          file_name = "tmp/alko_temp#{Time.now.to_i}.xlsx"
+          IO.copy_stream(URI.open(url), file_name)
 
-          # Remove unnecessary lines from the CSV
-          dest = File.open('data/alko.csv','w')
-          File.open('tmp/alko_temp.csv', 'r').each_with_index do |line, i|
-            next if i == 0 || i == 1 || i == 2
-            dest.write(line)
+          if File.exist?(temp_file) && FileUtils.identical?(file_name, temp_file)
+            File.delete(file_name)
+            puts "No update needed for data/alko.csv."
+          else
+            File.rename(file_name, temp_file)
+
+            sheet = Roo::Spreadsheet.open(temp_file)
+            sheet.to_csv('tmp/alko_temp.csv')
+
+            # Remove unnecessary lines from the CSV
+            dest = File.open('data/alko.csv','w')
+            File.open('tmp/alko_temp.csv', 'r').each_with_index do |line, i|
+              next if i == 0 || i == 1 || i == 2
+              dest.write(line)
+            end
+            dest.close
+            File.delete('tmp/alko_temp.csv')
+
+            puts "Alko's alcohol database loaded into data/alko.csv."
           end
-          dest.close
-
-          puts "Alko's alcohol database loaded into data/alko.csv."
           sleep(86400)
         end
       end
