bf85bbf3279928a97a10ae75860c2b7e03eece01
Luukuton <mleinikka@gmail.com>
2020-06-13 16:07:15 +0300

    Redesigned help and info commands + removed stars command

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ Rails web interface for the bot here: [TohsakaWeb](https://github.com/Luukuton/T
 ## Dependencies
 * Ruby >= 2.6 supported
 * MariaDB / MySQL 
-* Newest discordrb master branch
-* Other gems specified in Gemfile
+* Gems specified in Gemfile
+  * Latest [discordrb](https://github.com/discordrb/discordrb) @ master branch
 
 ## Installation & Usage
-
-1. Use these SQL commands to create user and database for the bot. Remember to change USERNAMEs and PASSWORD. 
+1. Install Ruby and MariaDB
+2. Use these SQL commands to create user and database for the bot. Remember to change USERNAMEs and PASSWORD. 
 ```
 CREATE USER 'USERNAME'@'localhost' IDENTIFIED BY 'PASSWORD';
 CREATE DATABASE IF NOT EXISTS tohsaka CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
@@ -19,6 +19,6 @@ GRANT ALL PRIVILEGES on tohsaka.* to 'USERNAME'@'localhost';
 FLUSH privileges;
 ```
 
-2. Switch to the root folder of the bot and run `bundle install` to install required gems.
-3. Run the `lib/first_time_setup.rb`.
-4. Start the bot by running `bundle exec ruby run.rb`.
+3. Switch to the root folder of the bot and run `bundle install` to install required gems.
+4. Run the `lib/first_time_setup.rb`.
+5. Start the bot by running `bundle exec ruby run.rb`.
--- lib/commands/other/info.rb
+++ lib/commands/other/info.rb
@@ -5,11 +5,22 @@ module TohsakaBot
       command(:info,
               aliases: %i[information],
               description: 'Basic information about the bot.',
-              usage: '',
-              rescue: "Something went wrong!\n`%exception%`") do |event|
+              usage: '') do |event|
 
-        event.channel.send_file(File.open("img/rin/rin_blush.gif"))
-        event.<< "A Discord bot created by Luukuton#8888 with Ruby."
+        event.channel.send_embed do |embed|
+          embed.title = "INFO"
+          embed.colour = 0xA82727
+          embed.url = ""
+          embed.description = ""
+          embed.timestamp = Time.now
+
+          embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://cdn.discordapp.com/attachments/351170098754486289/648936828212215812/22_1602-4fe170.gif")
+          embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: "Rin", icon_url: "https://cdn.discordapp.com/attachments/351170098754486289/648936891890008120/22_1615-a1fef0.png")
+          embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: "Made with Ruby", icon_url: "https://cdn.discordapp.com/emojis/232899886419410945.png")
+
+          embed.add_field(name: "Created by", value: "Luukuton#3717")
+          embed.add_field(name: "Source code", value: "[GitHub](https://github.com/Luukuton/TohsakaBot)")
+        end
       end
     end
   end
