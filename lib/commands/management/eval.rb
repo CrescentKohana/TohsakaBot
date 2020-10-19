d3c24e068c950aadae02fc1644b54640272109ec
Luukuton <mleinikka@gmail.com>
2020-10-19 03:01:53 +0300

    Fixed trigger_add as the way to call download_reply_picture was changed

--- lib/commands/management/eval.rb
+++ lib/commands/management/eval.rb
@@ -4,9 +4,10 @@ module TohsakaBot
       extend Discordrb::Commands::CommandContainer
       command(:eval,
               aliases: %i[breakstuff],
-              description: 'DANGER | 危険です。',
+              description: 'Run code. Only for the owner.',
               help_available: false) do |event, *code|
 
+        # Hard coded to allow ONLY the owner to have access.
         break unless event.user.id == AUTH.owner_id.to_i
         begin
           eval code.join(' ')
