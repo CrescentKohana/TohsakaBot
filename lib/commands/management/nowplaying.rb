b2d65f9bb8f0edf1591914dcdcc1873bd96b5b08
Luukuton <mleinikka@gmail.com>
2020-10-20 13:45:38 +0300

    Added 'search by ID' to the trigger search

--- lib/commands/management/nowplaying.rb
+++ lib/commands/management/nowplaying.rb
@@ -7,7 +7,7 @@ module TohsakaBot
               description: 'Now playing status.',
               usage: 'np <twitch url for streaming status> <status>',
               min_args: 1,
-              permission_level: 750) do |event, *m|
+              permission_level: 500) do |event, *m|
 
         np = m.join(' ').to_s
         cfg = YAML.load_file('cfg/config.yml')
