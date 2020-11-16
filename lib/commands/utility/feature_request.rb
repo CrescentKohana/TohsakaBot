591d2889d84cfd9dd780b09952f9be2012b06af2
Luukuton <mleinikka@gmail.com>
2020-11-16 15:46:34 +0200

    Added command featurerequest

---
+++ lib/commands/utility/feature_request.rb
@@ -0,0 +1,38 @@
+module TohsakaBot
+  module Commands
+    module FeatureRequest
+      extend Discordrb::Commands::CommandContainer
+      command(:featurerequest,
+              aliases: %i[requestfeature],
+              description: 'Takes an idea and saves it to file.',
+              usage: "Use 'triggeradd <feature or idea>",
+              min_args: 1,
+              require_register: true,
+              enabled_in_pm: false) do |event, *msg|
+
+        if TohsakaBot.user_limit_reached?(event.author.id, 1000, :triggers)
+          event.respond "The the maximum amount of feature requests a user can have is 1000. "
+          break
+        end
+
+        discord_uid = event.message.user.id
+        time = Time.parse(event.message.timestamp.to_s).to_i
+        request = msg.join(' ').strip
+
+        db = YAML::Store.new('data/feature_requests.yml')
+        db.transaction do
+          i = 1
+          i += 1 while db.root?(i)
+          db[i] = {
+              "user" => discord_uid,
+              "time" => time,
+              "request" => request
+          }
+          db.commit
+        end
+
+        event.respond("Request saved.")
+      end
+    end
+  end
+end
