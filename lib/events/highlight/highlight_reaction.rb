3479a54fa57032582b5cd02320ac5a54bc38c612
Luukuton <mleinikka@gmail.com>
2020-12-26 03:21:50 +0200

    Added prune (delete 2-100 msgs) and convertpins commands + made highlight function better

--- lib/events/highlight/highlight_reaction.rb
+++ lib/events/highlight/highlight_reaction.rb
@@ -3,9 +3,11 @@ module TohsakaBot
     module HighlightReaction
       extend Discordrb::EventContainer
       reaction_add(emoji: '📌') do |event|
+        next if event.channel.pm?
+
         highlight_core = HighlightCore.new(event.message, event.server.id, event.channel.id)
         next unless highlight_core.requirements_for_pin_met?
-        highlight_core.send_highlight
+        highlight_core.store_highlight(highlight_core.send_highlight)
       end
     end
   end
