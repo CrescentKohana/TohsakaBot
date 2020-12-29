245ce8c8388f6743b0687072c772f570334fefc6
Luukuton <mleinikka@gmail.com>
2020-12-29 20:39:16 +0200

    Adjusted alko command to the new source data format

--- lib/commands/utility/alko.rb
+++ lib/commands/utility/alko.rb
@@ -2,14 +2,15 @@ module TohsakaBot
   module Commands
     module Alko
       extend Discordrb::Commands::CommandContainer
+      max_budget = 500.freeze # in euros
       command(:alko,
               aliases: %i[drinks alcohol],
               description: 'Recommends drinks from Alko (a Finnish alcohol store) based on budget and type.',
-              usage: 'alko <max price in euros (integer, <=500€)> <type>',
+              usage: "alko <max price in euros (integer, <= #{max_budget}€)> <type>",
               min_args: 2) do |event, price, *type|
 
         m = event.respond('Parsing data...')
-        if price.to_i <= 500
+        if price.to_i <= max_budget
           aliases = YAML.load(File.read('data/persistent/alko_aliases.yml'))
           aliases.keys.each do |k|
             aliases[k].each do |v|
@@ -25,7 +26,7 @@ module TohsakaBot
           csv.map do |h|
             next if h['Nimi'].nil?
             if h['Tyyppi'].is_a?(String)
-              if (BigDecimal(h['Hinta']) * 100).to_i <= (price.to_f * 100).to_i && h['Tyyppi'].downcase == @output_type
+              if (BigDecimal(h['Hinta'], 0) * 100).to_i <= (price.to_f * 100).to_i && h['Tyyppi'].downcase == @output_type
                 matched << h
               end
             end
@@ -33,34 +34,39 @@ module TohsakaBot
 
           if matched.empty?
             m.delete
-            event.respond "No drinks were found within that budget or given type doesn't exist."
+            event.respond "No drinks were found within the budget or the given type doesn't exist."
             break
           end
 
           random_recommendations = []
           (1..5).each do |i|
-            random_recommendations << matched.sample(5).slice!(i)
+            random_recommendations << matched.sample(6).slice!(i)
           end
 
           alko_url = "https://www.alko.fi/tuotteet/"
 
-          sorted = random_recommendations.sort { |a, b| (BigDecimal(a[4]).to_i * 100) <=> (BigDecimal(b[4]).to_i * 100) }
+          sorted = random_recommendations.sort { |a, b| (BigDecimal(a[4], 0).to_i * 100) <=> (BigDecimal(b[4], 0).to_i * 100) }
           event.channel.send_embed do |embed|
             embed.title = "Here's something for you to ~~get drunk~~ enjoy"
             embed.colour = 0xA82727
             embed.timestamp = Time.now
             embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: 'Sorted by the ratio of alcohol-% to price (€)')
 
-            embed.add_field(name: ":black_small_square: #{sorted[0][1]} (#{sorted[0][3]} / #{sorted[0][20]}% / #{sorted[0][4]}€)", value: "#{alko_url}#{sorted[0][0]}")
-            embed.add_field(name: ":black_small_square: #{sorted[1][1]} (#{sorted[1][3]} / #{sorted[1][20]}% / #{sorted[1][4]}€)", value: "#{alko_url}#{sorted[1][0]}")
-            embed.add_field(name: ":black_small_square: #{sorted[2][1]} (#{sorted[2][3]} / #{sorted[2][20]}% / #{sorted[2][4]}€)", value: "#{alko_url}#{sorted[2][0]}")
-            embed.add_field(name: ":black_small_square: #{sorted[3][1]} (#{sorted[3][3]} / #{sorted[3][20]}% / #{sorted[3][4]}€)", value: "#{alko_url}#{sorted[3][0]}")
-            embed.add_field(name: ":black_small_square: #{sorted[4][1]} (#{sorted[4][3]} / #{sorted[4][20]}% / #{sorted[4][4]}€)", value: "#{alko_url}#{sorted[4][0]}")
+            embed.add_field(name: ":black_small_square: #{sorted[0][1]}",
+                            value: "[#{sorted[0][3]}・#{sorted[0][21]}%・#{sorted[0][4]}€](#{alko_url}#{sorted[0][0]})")
+            embed.add_field(name: ":black_small_square: #{sorted[1][1]}",
+                            value: "[#{sorted[1][3]}・#{sorted[1][21]}%・#{sorted[1][4]}€](#{alko_url}#{sorted[1][0]})")
+            embed.add_field(name: ":black_small_square: #{sorted[2][1]}",
+                            value: "[#{sorted[2][3]}・#{sorted[2][21]}%・#{sorted[2][4]}€](#{alko_url}#{sorted[2][0]})")
+            embed.add_field(name: ":black_small_square: #{sorted[3][1]}",
+                            value: "[#{sorted[3][3]}・#{sorted[3][21]}%・#{sorted[3][4]}€](#{alko_url}#{sorted[3][0]})")
+            embed.add_field(name: ":black_small_square: #{sorted[4][1]}",
+                            value: "[#{sorted[4][3]}・#{sorted[4][21]}%・#{sorted[4][4]}€](#{alko_url}#{sorted[4][0]})")
           end
-          m.edit "Data parsed. It took #{Time.now - event.timestamp} seconds."
+          m.edit "Data parsed in #{(Time.now - event.timestamp).truncate(2)}s"
         else
           m.delete
-          event.respond('The maximum budget is 500 euros.')
+          event.respond("The maximum budget is #{max_budget} euros.")
         end
       end
     end
