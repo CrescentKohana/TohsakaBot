c85853e9e9443fae0a83b796b6a58739b6c98ec7
Luukuton <mleinikka@gmail.com>
2021-03-03 19:03:21 +0200

    Number command returns the range used

--- lib/commands/fun/number.rb
+++ lib/commands/fun/number.rb
@@ -11,11 +11,9 @@ module TohsakaBot
 
         name = BOT.member(event.server, event.author.id).display_name.strip_mass_mentions.sanitize_string
 
-        # Ruby 2.6.0 enables casting to an integer without raising an exception, and will return nil if the cast fails.
-        # https://rubyreferences.github.io/rubychanges/2.6.html#numeric-methods-have-exception-argument
         if (one.nil? && two.nil?) || (!Integer(one, exception: false) || !Integer(two, exception: false))
-          event.<< "**#{rand(0..9)}**  `#{name}`"
-          break
+          one = 0
+          two = 9
         end
 
         if one.to_i < -100_000_000 || two.to_i > 100_000_000
@@ -26,7 +24,13 @@ module TohsakaBot
         two, one = one, two if one.to_i > two.to_i
 
         number = rand(one.to_i..two.to_i)
-        event.<< "**#{number}**  `#{name}`"
+        event.channel.send_embed do |embed|
+          embed.colour = 0x36393F
+          embed.add_field(
+            name: "**#{number}** 🎲 (#{one}..#{two})",
+            value: "[#{name}](https://discord.com/channels/#{event.server.id}/#{event.channel.id}/#{event.message.id})"
+          )
+        end
       end
     end
   end
