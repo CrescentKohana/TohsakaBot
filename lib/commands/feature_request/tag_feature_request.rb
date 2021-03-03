d35fb8ea94202d7ea2f2d20f855827e2a88f885d
Luukuton <mleinikka@gmail.com>
2021-03-03 19:52:47 +0200

    Feature request notification when tagged

--- lib/commands/feature_request/tag_feature_request.rb
+++ lib/commands/feature_request/tag_feature_request.rb
@@ -5,21 +5,34 @@ module TohsakaBot
       command(:tagfeaturerequest,
               aliases: %i[tagfr],
               description: 'Tags a feature request. Currently all tags (new, indev, done, wontdo) are exclusive from each other.',
-              usage: "Use 'fr <tags (new, indev, done, wontdo)>'",
+              usage: "Use 'fr <tag (new, indev, done, wontdo)>'",
               min_args: 2,
               require_register: true,
-              permission_level: 1000) do |event, id, *tags|
+              permission_level: 1000) do |event, id, tag|
 
         requests = YAML.load_file('data/feature_requests.yml')
 
         if requests[id.to_i].nil?
           event.<< "No feature request with an ID of `#{id}` found."
         else
-          requests[id.to_i]['tags'] = tags.join(' ')
-          File.open('data/feature_requests.yml', 'w') do |h|
-            h.write requests.to_yaml
-          end
+          requests[id.to_i]['tags'] = tag
+          File.open('data/feature_requests.yml', 'w') { |h| h.write requests.to_yaml }
           event.<< "Feature request `#{id}` updated!"
+
+          msg = if tag == "done"
+                  "is done!\n`#{requests[id.to_i]["request"]}`"
+                elsif tag == "indev"
+                  "is in development!\n`#{requests[id.to_i]["request"]}`"
+                elsif tag == "wontdo"
+                  "was declined: `#{requests[id.to_i]["request"]}`"
+                else
+                  nil
+                end
+
+          break if msg.nil?
+
+          username = "by <@!#{requests[id.to_i]["user"]}>"
+          BOT.channel(CFG.default_channel.to_i).send("Feature request `#{id}` #{username} #{msg}")
         end
       end
     end
