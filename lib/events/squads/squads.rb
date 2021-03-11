6e659beafb5d32679e365a1c4a3250095ec74194
Luukuton <mleinikka@gmail.com>
2021-03-11 16:45:58 +0200

    Custom group sizes for Squads

--- lib/events/squads/squads.rb
+++ lib/events/squads/squads.rb
@@ -21,6 +21,8 @@ module TohsakaBot
 
         author_id = event.message.author.id.to_i
         reactions = event.message.reactions.map { |r| { r.name => r.count } }
+        custom_group_size = event.message.content.sub(/<@&\d*>/, "").first_number
+
         roles.each_key do |role|
           members = JSON.parse(Discordrb::API::Channel.get_reactions(
             "Bot #{AUTH.bot_token}", event.channel.id, event.message.id, "✅", false, false
@@ -28,7 +30,9 @@ module TohsakaBot
 
           reaction_count = reactions[0]["✅"].to_i
           reaction_count -= 1 if members.include? "<@!#{author_id}>"
-          next unless role_mentions.key?(roles[role]["role_id"]) && reaction_count >= roles[role]["group_size"]
+          group_size = custom_group_size.nil? ? roles[role]["group_size"] : custom_group_size
+
+          next unless role_mentions.key?(roles[role]["role_id"]) && reaction_count >= group_size.to_i
 
           event.respond "Found squad for **#{role}**: <@!#{author_id}> #{members.join(' ')}"
           event.message.delete
