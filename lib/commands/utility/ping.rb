f1d0c703066b269850ace7f64a12c033947b389c
Luukuton <mleinikka@gmail.com>
2021-03-11 17:50:23 +0200

    Command: SetLanguage

--- lib/commands/utility/ping.rb
+++ lib/commands/utility/ping.rb
@@ -6,9 +6,14 @@ module TohsakaBot
               description: I18n.t(:'commands.utility.ping.description'),
               usage: I18n.t(:'commands.utility.ping.usage')) do |event|
 
+        now = Time.now
         locale = TohsakaBot.get_locale(event.user.id)
         event.respond(
-          I18n.t(:'commands.utility.ping.response', locale: locale.to_sym, time: Time.now - event.timestamp)
+          I18n.t(
+            :'commands.utility.ping.response',
+            locale: locale.to_sym,
+            time: ((now - event.timestamp) * 1000).truncate
+          )
         )
       end
     end
