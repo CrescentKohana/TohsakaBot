2bd96d7d682607bba15027d736ab460f1fa5a31a
Luukuton <mleinikka@gmail.com>
2021-06-21 15:12:57 +0300

    Don't delete reminder_add commands on error

--- lib/commands/logic/reminder/reminder_add.rb
+++ lib/commands/logic/reminder/reminder_add.rb
@@ -13,7 +13,7 @@ module TohsakaBot
 
       def run
         if @datetime.blank? && (!@msg.blank? || !@repeat.blank?)
-          return { content: I18n.t(:'commands.reminder.add.errors.all_blank'), embeds: nil }
+          return { content: I18n.t(:'commands.reminder.add.errors.all_blank'), embeds: nil, error: true }
         end
 
         # Legacy logic for simple reminders through text commands
@@ -35,16 +35,16 @@ module TohsakaBot
         begin
           ReminderHandler.handle_user_limit(TohsakaBot.command_event_user_id(@event))
         rescue ReminderHandler::UserLimitReachedError => e
-          return { content: e.message, embeds: nil }
+          return { content: e.message, embeds: nil, error: true }
         end
 
         begin
           rem.convert_datetime
         rescue ReminderHandler::DatetimeError, ReminderHandler::RepeatIntervalError => e
-          return { content: e.message, embeds: nil }
+          return { content: e.message, embeds: nil, error: true }
         end
 
-       { content:  rem.store_reminder, embeds: nil }
+       { content: rem.store_reminder, embeds: nil, error: false }
       end
     end
   end
