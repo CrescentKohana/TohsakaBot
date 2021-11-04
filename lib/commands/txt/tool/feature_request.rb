70fdd5ccc5aca6b09da42766b3a8f8ff81a02e89
Marko Leinikka <Luukuton@users.noreply.github.com>
2021-11-04 22:57:47 +0200

    Refactor feature requests to use SQL

--- lib/commands/txt/tool/feature_request.rb
+++ lib/commands/txt/tool/feature_request.rb
@@ -17,51 +17,46 @@ module TohsakaBot
         #   break
         # end
 
-        discord_uid = event.message.user.id
-        time = Time.parse(event.message.timestamp.to_s).to_i
-        request = msg.join(' ').strip
+        content = msg.join(' ').strip
 
-        db = YAML::Store.new('data/feature_requests.yml')
-        i = 1
-        db.transaction do
-          i += 1 while db.root?(i)
-          db[i] = {
-            'tags' => 'new',
-            'user' => discord_uid,
-            'time' => time,
-            'request' => request
-          }
-          db.commit
+        issues = TohsakaBot.db[:issues]
+        TohsakaBot.db.transaction do
+          @id = issues.insert(
+            content: content,
+            type: 0,
+            status: 'new',
+            user_id: TohsakaBot.get_user_id(event.message.user.id),
+            created_at: Time.now,
+            updated_at: Time.now
+          )
         end
 
-        event.respond("Request saved `<ID: #{i}>`.")
+        event.respond("Request saved `<ID: #{@id}>`.")
       end
 
       command(:findfeaturerequests,
               aliases: %i[findfrs findfr findfeaturerequest ffr requests frs],
-              description: 'Finds and lists feature requests based on given tags.',
-              usage: "ffr <tags (new, indev, done, wontdo, all)>",
+              description: 'Finds and lists feature requests based on given status.',
+              usage: "ffr <status (new, indev, done, wontdo, all)>",
               min_args: 1,
-              require_register: true) do |event, *tags|
+              require_register: true) do |event, status|
         result_amount = 0
         header = "`  ID | CREATED    | BY                               | TAGS `\n".dup
         output = ''.dup
-        requests = YAML.safe_load(File.read('data/feature_requests.yml'))
+        issues = if status == "all"
+                   TohsakaBot.db[:issues].order(:created_at)
+                 else
+                   TohsakaBot.db[:issues].order(:created_at).where(status: status)
+                 end
 
-        if requests
-          sorted = requests.sort
-          sorted.each do |id, r|
-            next unless tags.any? { |tag| r['tags'].include? tag } || tags.include?('all')
-
-            result_amount += 1
-            datetime = Time.at(r['time']).to_s.split(' ')[0]
-            username = BOT.user(r['user']).username
-            output << "`#{format('%4s', id)} | #{datetime} |"\
-                      " #{format('%-32s', username)} | #{r['tags']}`\n`\t\tREQ:` #{r['request']}\n"
-          end
+        issues.each do |issue|
+          datetime = Time.at(issue[:created_at]).to_s.split(' ')[0]
+          username = BOT.user(issue[:user_id]).username
+          output << "`#{format('%4s', id)} | #{datetime} |"\
+                    " #{format('%-32s', username)} | #{issue[:status]}`\n`\t\tREQ:` #{issue[:content]}\n"
         end
 
-        where = result_amount > 5 ? event.author.pm : event.channel
+        where = issues.size > 5 ? event.author.pm : event.channel
         msgs = []
         if result_amount.positive?
           header << output
@@ -80,29 +75,34 @@ module TohsakaBot
               usage: "tag <id> <tag (new, indev, done, wontdo)>",
               min_args: 2,
               require_register: true,
-              permission_level: TohsakaBot.permissions.actions["feature_requests"]) do |event, id, tag|
-        requests = YAML.load_file('data/feature_requests.yml')
+              permission_level: TohsakaBot.permissions.actions["feature_requests"]) do |event, id, status|
+        issues = TohsakaBot.db[:issues]
+        issue = issues.where(id: id.to_i).single_record!
 
-        if requests[id.to_i].nil?
+        if issue.nil?
           event.<< "No feature request with an ID of `#{id}` found."
         else
           msg = case tag
                 when "done"
-                  "is done!\n`#{requests[id.to_i]['request']}`"
+                  "is done!\n`#{issue[:content]}`"
                 when "indev"
-                  "is in development!\n`#{requests[id.to_i]['request']}`"
+                  "is in development!\n`#{issue[:content]}`"
                 when "wontdo"
-                  "was declined: `#{requests[id.to_i]['request']}`"
+                  "was declined: `#{issue[:content]}`"
                 else
                   break
                 end
           break if msg.nil?
 
-          requests[id.to_i]['tags'] = tag
-          File.open('data/feature_requests.yml', 'w') { |h| h.write requests.to_yaml }
+          issue[:status] = status
+
+          TohsakaBot.db.transaction do
+            issues.where(id: id.to_i).update(issue)
+          end
+
           event.<< "Feature request `#{id}` updated!" unless event.channel.id.to_i == CFG.default_channel.to_i
 
-          username = "by <@!#{requests[id.to_i]['user']}>"
+          username = "by <@!#{TohsakaBot.get_discord_id(issue[:user_id])}>"
           BOT.channel(CFG.default_channel.to_i).send_message("Feature request `#{id}` #{username} #{msg}")
         end
       end
