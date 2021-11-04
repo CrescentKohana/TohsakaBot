00a136eaa38b6a9c1bb91d8fddda91c3c9e0f8d0
Marko Leinikka <Luukuton@users.noreply.github.com>
2021-11-05 01:55:13 +0200

    Convert Trophy roles (Fool, Winner) to SQL

--- lib/commands/txt/role/mvp.rb
+++ lib/commands/txt/role/mvp.rb
@@ -12,9 +12,9 @@ module TohsakaBot
               permission_level: TohsakaBot.permissions.actions["trophy_roles"]) do |event, _user, days, *reason|
         user_id = event.message.mentions[0].id
 
-        TohsakaBot.give_temporary_role(event, CFG.mvp_role, user_id, days, reason)
+        TohsakaBot.give_trophy(event, CFG.mvp_role, user_id, days, reason)
         event.respond(
-          "#{event.message.mentions[0].username} has been rewarded the rank of MVP for #{days} day#{'s' if days.to_i > 1}."
+          "#{event.message.mentions[0].display_name} has been rewarded the rank of MVP for #{days} day#{'s' if days.to_i > 1}."
         )
       end
     end
--- lib/helpers/discord_helper.rb
+++ lib/helpers/discord_helper.rb
@@ -64,52 +64,30 @@ module TohsakaBot
       user_msg&.delete
     end
 
-    def give_temporary_role(event, role_id, user_id, days, reason)
-      db_store = YAML::Store.new('data/temporary_roles.yml')
+    def give_trophy(event, role_id, user_id, days, reason)
       server_id = event.channel.server.id
       role_id = role_id.to_i
 
-      # If the user already has an entry for the role, this deletes it first.
-      # TohsakaBot.delete_temporary_role_db(user_id, role_id)
-
-      # Gives the role to the user unless they have it.
+      # Gives the role to the user unless they already have it.
       unless TohsakaBot::BOT.member(event.server, user_id)&.role?(role_id)
         Discordrb::API::Server.add_member_role("Bot #{AUTH.bot_token}", server_id, user_id, role_id)
       end
 
       days = Integer(days, exception: false)
       days = 7 if days.nil? || !days.between?(1, 365)
-
       reason = reason.join(' ').sanitize_string
 
       # Makes a new entry to the database for the user so that the role can be deleted after a set time.
-      # Default: 1 week
-      db_store.transaction do
-        i = 1
-        i += 1 while db_store.root?(i)
-        db_store[i] = {
-          'time' => Time.now,
-          'duration' => days,
-          'reason' => reason,
-          'user' => user_id,
-          'server' => server_id,
-          'role' => role_id
-        }
-        db_store.commit
-      end
-    end
-
-    def delete_temporary_role_db(user_id, role_id)
-      db_read = YAML.load_file('data/temporary_roles.yml')
-      db_store = YAML::Store.new('data/temporary_roles.yml')
-
-      db_read.each do |k, v|
-        next unless role_id == v['role'].to_i && user_id == v['user'].to_i
-
-        db_store.transaction do
-          db_store.delete(k)
-          db_store.commit
-        end
+      trophies = TohsakaBot.db[:trophies]
+      TohsakaBot.db.transaction do
+        @id = trophies.insert(
+          reason: reason,
+          duration: days,
+          category: 0,
+          discord_uid: user_id,
+          server_id: server_id,
+          role_id: role_id
+        )
       end
     end
 
