9b2bfc94c330863e19171667dea16aa8c649f87a
Marko Leinikka <mleinikka@gmail.com>
2021-11-18 20:10:03 +0200

    Fix Squad mute giving role 'back' to users that didn't have a role in the first place

--- lib/events/squads/squads_mute.rb
+++ lib/events/squads/squads_mute.rb
@@ -16,11 +16,13 @@ module TohsakaBot
         end
         next if role_id.nil?
 
+        previous_mute = false
         mute_db = YAML.load_file('data/squads_mute.yml')
         mute_db.each do |k, v|
           next if v.nil?
           next if v['role'].to_i != role_id || v['user'].to_i != event.user.id
 
+          previous_mute = true
           db = YAML::Store.new('data/squads_mute.yml')
           db.transaction do
             db.delete(k)
@@ -28,29 +30,31 @@ module TohsakaBot
           end
         end
 
-        durations = { "❌" => 1, "🚫" => 6, "🔕" => 24 }
-        db = YAML::Store.new("data/squads_mute.yml")
-        i = 1
-        db.transaction do
-          i += 1 while db.root?(i)
-          db[i] = {
-            'time' => Time.now,
-            'hours' => durations[event.emoji.name],
-            'user' => event.user.id,
-            'server' => event.server.id,
-            'role' => role_id
-          }
-          db.commit
+        if previous_mute || BOT.member(event.server.id, event.user.id)&.role?(role_id)
+          durations = { "❌" => 1, "🚫" => 6, "🔕" => 24 }
+          db = YAML::Store.new("data/squads_mute.yml")
+          i = 1
+          db.transaction do
+            i += 1 while db.root?(i)
+            db[i] = {
+              'time' => Time.now,
+              'hours' => durations[event.emoji.name],
+              'user' => event.user.id,
+              'server' => event.server.id,
+              'role' => role_id
+            }
+            db.commit
+          end
         end
 
-        next unless BOT.member(event.server.id, event.user.id)&.role?(role_id)
-
-        Discordrb::API::Server.remove_member_role(
-          "Bot #{AUTH.bot_token}",
-          event.channel.server.id,
-          event.user.id,
-          role_id
-        )
+        if BOT.member(event.server.id, event.user.id)&.role?(role_id)
+          Discordrb::API::Server.remove_member_role(
+            "Bot #{AUTH.bot_token}",
+            event.channel.server.id,
+            event.user.id,
+            role_id
+          )
+        end
       end
     end
   end
