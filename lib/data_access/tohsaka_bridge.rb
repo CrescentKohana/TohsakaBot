23c8010c5c78025f3cbd99624bd09e000d6e486b
Marko Leinikka <mleinikka@gmail.com>
2021-11-22 20:58:34 +0200

    Add uid parameter

--- lib/data_access/tohsaka_bridge.rb
+++ lib/data_access/tohsaka_bridge.rb
@@ -61,8 +61,8 @@ module TohsakaBot
       TohsakaBot.server_cache[server_id]
     end
 
-    def channel_permission?(server_id, channel_id, action)
-      BOT.member(server_id, discord_uid).permission?(action, BOT.channel(channel_id))
+    def channel_permission?(server_id, channel_id, discord_uid, action)
+      BOT.member(server_id, discord_uid)&.permission?(action, BOT.channel(channel_id))
     end
 
     # Checks that the member is in same server as the bot.
