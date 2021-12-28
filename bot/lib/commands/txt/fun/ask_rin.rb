af00802419e7c0ede908b05b719b22282e4ce742
Marko Leinikka <Luukuton@users.noreply.github.com>
2021-12-28 16:16:36 +0200

    feat(bot): simplify ask_rin

--- bot/lib/commands/txt/fun/ask_rin.rb
+++ bot/lib/commands/txt/fun/ask_rin.rb
@@ -8,24 +8,14 @@ module TohsakaBot
               aliases: %i[ask rin],
               description: 'Ask Rin about something and she will deliver.',
               usage: 'askrin <question>',
-              min_args: 1) do |event, *msg|
-        msg = msg.join(' ').sanitize_string
-        username = BOT.member(event.server, event.user.id).display_name.strip_mass_mentions.sanitize_string
-        answer = ' '
+              min_args: 1) do |event|
+        answer = ''
 
         CSV.open('data/ask_rin_answers.csv', 'r', col_sep: "\t") do |csv|
           answer = csv.read.sample[0]
         end
 
-        url = URI::DEFAULT_PARSER.make_regexp.match?(answer) ? true : false
-        event.channel.send_embed do |embed|
-          embed.image = Discordrb::Webhooks::EmbedImage.new(url: answer) if url
-          embed.colour = 0x36393F
-          embed.add_field(name: "**#{username}**: ", value: "[#{msg}](https://discord.com/channels/"\
-                                "#{event.server.id}/#{event.channel.id}/#{event.message.id})")
-          embed.add_field(name: '**Rin** (凛): ', value: answer.to_s) unless url
-          embed.add_field(name: '**Rin** (凛): ', value: '🖼️') if url
-        end
+        event.message.reply!(answer.to_s)
       end
     end
   end
