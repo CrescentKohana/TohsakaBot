7e627cbae96d5a4eabe52fd865523439f4b705a1
Marko Leinikka <mleinikka@gmail.com>
2021-12-31 00:48:06 +0200

    fix(web): callback URL protocol in omniauth

--- rails/config/initializers/omniauth.rb
+++ rails/config/initializers/omniauth.rb
@@ -2,12 +2,14 @@
 
 require 'omniauth-discord'
 
+protocol = Rails.configuration.user_config.ssl == 'true' ? 'https://' : 'http://'
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :discord,
            Rails.application.credentials.dig(:discord, :client_id),
            Rails.application.credentials.dig(:discord, :secret),
            scope: 'identify',
-           callback_url: "#{Rails.configuration.user_config.web_host}/auth/discord/callback"
+           callback_url: "#{protocol}#{Rails.configuration.user_config.web_host}/auth/discord/callback"
 end
 
 # Force redirection when failed OAuth. https://stackoverflow.com/a/11028187
