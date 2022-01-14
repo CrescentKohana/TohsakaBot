5f6fa195e0ac7bae1049e81c12bc8f8d95553e04
Marko Leinikka <Luukuton@users.noreply.github.com>
2022-01-14 13:37:39 +0200

    feat(bot): adjust timeout values

--- bot/lib/cron/jobs/expire_timeouts.rb
+++ bot/lib/cron/jobs/expire_timeouts.rb
@@ -4,11 +4,12 @@ module TohsakaBot
   module Jobs
     def self.expire_timeouts(now)
       TohsakaBot.timeouts_cache.timeouts.each do |id, timeout|
-        if (timeout[:created_at].to_i + 60 * 10) < now.to_i
+        if (timeout[:created_at].to_i + 60 * 5) < now.to_i
           TohsakaBot.timeouts_cache.timeouts.delete(id)
           next
         end
-        TohsakaBot.timeouts_cache.stop(id) if TohsakaBot.timeouts_cache.total_votes(id) >= 1
+        difference = timeout[:votes][:yes] - timeout[:votes][:no]
+        TohsakaBot.timeouts_cache.stop(id) if TohsakaBot.timeouts_cache.total_votes(id) > 6 && difference.abs > 2
       end
     end
   end
