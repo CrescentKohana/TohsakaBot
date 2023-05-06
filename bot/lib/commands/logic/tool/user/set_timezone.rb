b067ac0e1df8550879c4556a4594b1889a546fef
Marko Leinikka <mleinikka@gmail.com>
2023-05-07 01:58:03 +0900

    refactor: SetTimezone

--- bot/lib/commands/logic/tool/user/set_timezone.rb
+++ bot/lib/commands/logic/tool/user/set_timezone.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
-TimezoneKeys = ActiveSupport::TimeZone::MAPPING.keys.map(&:downcase).freeze
+TimezoneKeys = ActiveSupport::TimeZone::MAPPING.values.map(&:downcase).freeze
 
 module TohsakaBot
   module CommandLogic
     class SetTimezone
       def initialize(event, timezone)
         @event = event
-        @timezone = timezone.is_a?(String) ? timezone.downcase : timezone.key.downcase
+        @timezone = timezone.is_a?(String) ? timezone : timezone.value
       end
 
       def run
         user_id = TohsakaBot.command_event_user_id(@event)
-        unless TimezoneKeys.include? @timezone
+        unless TimezoneKeys.include? @timezone.downcase
           return { content: I18n.t(:'commands.tool.user.set_timezone.error.tz_not_found',
                                    locale: TohsakaBot.get_locale(user_id)) }
         end
 
         TohsakaBot.db.transaction do
-          TohsakaBot.db[:users].where(id: TohsakaBot.get_user_id(user_id)).update(timezone: @locale)
+          TohsakaBot.db[:users].where(id: TohsakaBot.get_user_id(user_id)).update(timezone: @timezone)
         end
-        { content: I18n.t(:'commands.tool.user.set_timezone.response', timezone: @timezone, locale: TohsakaBot.get_locale(user_id)) }
+        { content: I18n.t(:'commands.tool.user.set_timezone.response', timezone: @timezone) }
       end
     end
   end
